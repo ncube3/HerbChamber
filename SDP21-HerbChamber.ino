876411f857fb9e6e07799af6025c3070c9c482e0
Nam Nguyen <35734907+ncube3@users.noreply.github.com>
2021-04-01 18:06:11 -0400

    Update SDP21-HerbChamber.ino

--- SDP21-HerbChamber.ino
+++ SDP21-HerbChamber.ino
@@ -30,11 +30,8 @@ SoftwareSerial serialtoESP(9,10);
 const int waterTime = 5;          //should be 28
 
 
-const int moistureThreshold = 30;
+const int moistureThreshold = 20;
 const int tempThreshold = 80;
-
-const int minMoisture = 250;
-const int maxMoisture = 700;
 
 const int inputProcessInterval = 1;
 
@@ -58,7 +55,7 @@ void setup() {
   digitalWrite(RLY_PUMP, LOW);
   digitalWrite(RLY_FANS, LOW);
   digitalWrite(RLY_LIGHT, LOW);
-  digitalWrite(RLY_VALVE1, LOW);
+  digitalWrite(RLY_VALVE1, HIGH);
   digitalWrite(RLY_VALVE2, LOW);
   digitalWrite(RLY_VALVE3, LOW);
   digitalWrite(RLY_VALVE4, LOW);
@@ -70,6 +67,7 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Starting up");
   statusBlink(3);
+  allRelaysTest();
   
   //turning on the fan and light
   digitalWrite(RLY_LIGHT, HIGH);
@@ -96,11 +94,31 @@ void loop() {
 
 void processSensorInputs(){
 
+  int ma1 = 0;
+  int ma2 = 0;
+  int ma3= 0;
+  int ma4 = 0;
   
-  int ma1 = analogRead(SENSOR_MOISTURE1);   //make average of 100 measuremnts
-  int ma2 = analogRead(SENSOR_MOISTURE2);
-  int ma3 = analogRead(SENSOR_MOISTURE3);
-  int ma4 = analogRead(SENSOR_MOISTURE4);
+  for(int i = 0; i < 10; i++){
+     int a1 = analogRead(SENSOR_MOISTURE1);   //make average of 100 measuremnts
+    int a2 = analogRead(SENSOR_MOISTURE2);
+    int a3 = analogRead(SENSOR_MOISTURE3);
+    int a4 = analogRead(SENSOR_MOISTURE4);
+
+    ma1+=a1;
+    ma2+=a2;
+    ma3+=a3;
+    ma4+=a4;
+
+    
+    delay(10);
+ }
+
+ ma1/=10;
+ ma2/=10;
+ ma3/=10;
+ ma4/=10;
+  
   
   double temp = dht.readTemperature(true);
   double humid = dht.readHumidity();
@@ -113,10 +131,10 @@ void processSensorInputs(){
   
 
   
-  int m1 = map(ma1, maxMoisture, minMoisture, 0, 100)-14;
-  int m2 = map(ma2, maxMoisture, minMoisture, 0, 100)-122;
-  int m3 = map(ma3, maxMoisture, minMoisture, 0, 100)-17;
-  int m4 = map(ma4, maxMoisture, minMoisture, 0, 100)+9;
+  int m1 = map(ma1, 630, 350, 0, 100);
+  int m2 = map(ma2, 610, 355, 0, 100);
+  int m3 = map(ma3, 610, 365, 0, 100);
+  int m4 = map(ma4, 740, 495, 0, 100);
 
 
   // Make decisions based on threshold
@@ -159,7 +177,7 @@ void processSensorInputs(){
   String moisture2 = (String)m2;
   String moisture3 =(String)m3;
   String moisture4 = (String)m4;
-  String serialOut = "| "+humid1+" | " +temp1+" | "+m1+"  "+m2+"  "+m3+"  "+m4+"                   ";
+  String serialOut = "| "+humid1+" | " +temp1+" | "+m1+" "+m2+" "+m3+" "+m4+"                             ";
   Serial.println(serialOut);
   serialtoESP.print(serialOut);
 
@@ -182,12 +200,94 @@ void selectValve(int valveNum){
   int selectedValve = RLY_VALVE1 + (valveNum-1);
 
   
-  digitalWrite(RLY_VALVE1, LOW);
+  digitalWrite(RLY_VALVE1, HIGH);
   digitalWrite(RLY_VALVE2, LOW);
   digitalWrite(RLY_VALVE3, LOW);
   digitalWrite(RLY_VALVE4, LOW);
-  
-  digitalWrite(selectedValve, HIGH);
 
+  if(selectedValve == RLY_VALVE1)
+    digitalWrite(selectedValve, LOW);
+  else
+    digitalWrite(selectedValve, HIGH);
 }
 
+
+
+
+void allRelaysTest(){
+
+  
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(2, HIGH);
+  delay(2000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(2, LOW);
+  delay(2000);
+
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(3, HIGH);
+  delay(2000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(3, LOW);
+  delay(2000);
+
+  
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(4, HIGH);
+  delay(2000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(4, LOW);
+  delay(2000);
+
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(5, HIGH);
+  delay(2000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(5, LOW);
+  delay(2000);
+
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(6, HIGH);
+  delay(2000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(6, LOW);
+  delay(2000);
+
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(7, HIGH);
+  delay(2000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(7, LOW);
+  delay(2000);
+
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(8, HIGH);
+  delay(2000);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(8, LOW);
+  delay(2000);
+
+  statusBlink(3);
+
+}
